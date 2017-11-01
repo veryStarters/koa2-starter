82a8463ae7d17afadae08c51a2f966d1d695e061
taoql <taoql@gold-finance.com.cn>
2017-11-01 15:32:01 +0800

    新增默认及404路由，

--- template/src/controllers/user/new.js
+++ template/src/controllers/user/new.js
@@ -2,6 +2,12 @@
  * 本文件为接口定义文件，需export一个普通方法或者一个async方法，ctx参数为接口上下文环境，从中可以获取到本次请求的相关参数
  * 设置ctx.body即可向客户端response数据
  */
-export default ctx => {
-  ctx.body= 'hello, userNew! '
+import UserModel from 'models/user'
+export default async ctx => {
+  let user = new UserModel({
+    name: 'taoqili',
+    age: Math.random()
+  })
+  user.save()
+  ctx.body = '创建成功'
 }
--- template/src/middlewares/index.js
+++ template/src/middlewares/index.js
@@ -6,6 +6,6 @@ export const RequestLog = async (ctx, next) => {
   const start = new Date()
   await next()
   const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url}  --  ${ms}ms`)
+  console.log(`${ctx.method} ${ctx.url} ${ctx.status} ${ms}ms ${JSON.stringify(ctx.request.query)}`)
   console.log(`${JSON.stringify(ctx.response.body).replace(/\\/g, '')}`)
 }
