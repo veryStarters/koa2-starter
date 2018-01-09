af80b410071d82b5173d3661182c0d4277a29171
taoqili <taoqili@126.com>
2018-01-09 17:02:45 +0800

    更新： 重构路由,增加dispatch模块，controllers中的index.js将作为对应路由的入口文件

--- template/src/initRouter.js
+++ template/src/initRouter.js
@@ -8,9 +8,10 @@ export default (app, options) => {
     viewRoot: path.join(__dirname, 'pages/'),
     viewExt: 'html'
   }, options || {})
-  let koaRouter = new KoaRouter({
-    prefix: opt.apiPrefix
-  })
+  let koaRouter = new KoaRouter()
+  const emptyAction = async (ctx, next) => {
+    await next()
+  }
   // 接口层次的私有middleWares只use到最顶层的路由即可
   const hasUsedMiddleWares = (() => {
     let routePaths = []
@@ -22,30 +23,47 @@ export default (app, options) => {
         }
       })
       routePaths.push(routePath)
-      console.log(JSON.stringify(routePaths))
       return ret
     }
   })()
-  walk(opt.apiRoot)
+  // 处理页面路由
+  koaRouter.get('/*', async (ctx, next) => {
+    let reqPath = ctx.request.path
+    if(reqPath === '/') {
+      reqPath = '/index'
+    }
+    let query = ctx.request.query
+    if (reqPath.indexOf(opt.apiPrefix) === 0) {
+      return next()
+    }
+    if (!fs.existsSync(path.join(opt.viewRoot, `${reqPath}.${opt.viewExt}`))) {
+      return next()
+    }
+    await ctx.render(reqPath.substr(1), query)
+  })
+  koaRouter.all('/api', async (ctx, next) => {
+    ctx.body = 'API List'
+  })
+  // 注册所有API路由
+  registerApiRouter(opt.apiRoot)
+
+  // 处理404
   koaRouter.all('*', async (ctx) => {
     ctx.status = 404
-    ctx.body = '404'
+    await ctx.render('404')
   })
   app.use(koaRouter.routes())
   app.use(koaRouter.allowedMethods())
 
-  async function emptyAction (ctx, next) {
-    await next()
-  }
   function formatPath (fileName) {
     return fileName.replace(/.*\/controllers\//, '').replace(/\/index\.js$/, '');
   }
 
   function fixRoutePath(routePath, routeName) {
     if (routeName === 'index' || routeName === 'default') {
-      return `/${routePath}`
+      return `${opt.apiPrefix}/${routePath}`
     } else {
-      return `/${routePath}/${routeName}`
+      return `${opt.apiPrefix}/${routePath}/${routeName}`
     }
   }
 
@@ -57,17 +75,17 @@ export default (app, options) => {
     }
   }
 
-  function walk(fileDir) {
-    let fileList = fs.readdirSync(fileDir)
-    fileList.forEach(function (file) {
-      let fileName = path.join(fileDir, file)
-      let stat = fs.statSync(fileName)
+  function registerApiRouter(apiDir) {
+    let apiList = fs.readdirSync(apiDir)
+    apiList.forEach(function (api) {
+      let apiFileName = path.join(apiDir, api)
+      let stat = fs.statSync(apiFileName)
       if (stat.isFile()) {
-        if (/index\.js$/.test(fileName)) {
-          let routes = require(fileName)
+        if (/index\.js$/.test(apiFileName)) {
+          let routes = require(apiFileName)
           let middlewares = routes['middlewares'] || []
           Object.keys(routes).forEach(routeName => {
-            let routePath = formatPath(fileName)
+            let routePath = formatPath(apiFileName)
             if (routeName === 'middlewares') {
               return
             }
@@ -77,14 +95,14 @@ export default (app, options) => {
               setMiddleWare(middlewares, routePath)
               koaRouter.all(routePath, routeInfo)
             } else if (typeof routeInfo === 'object') {
-              routePath = routeInfo.route || fixRoutePath(routePath, routeName)
+              routePath = `${opt.apiPrefix}${routeInfo.route}` || fixRoutePath(routePath, routeName)
               setMiddleWare(middlewares, routePath)
               koaRouter[routeInfo.method ? routeInfo.method : 'post'](routePath, routeInfo.action || emptyAction)
             }
           })
         }
       } else if (stat.isDirectory()) {
-        walk(fileName)
+        registerApiRouter(apiFileName)
       }
     })
   }
